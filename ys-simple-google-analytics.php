bd2e7f7c9aebb70d4e066944014573c691ee554e
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-07-14 03:00:59 +0900

    Update documents

--- ys-simple-google-analytics.php
+++ ys-simple-google-analytics.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Ys Simple Google Analytics
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/yosiakatsuki/ys-simple-google-analytics
+ * Description:     This plugin enables Google Analytics for your WordPress site.
+ * Author:          yosiakatsuki
+ * Author URI:      https://yosiakatsuki.net/
  * Text Domain:     ys-simple-google-analytics
  * Domain Path:     /languages
  * Version:         0.1.0
