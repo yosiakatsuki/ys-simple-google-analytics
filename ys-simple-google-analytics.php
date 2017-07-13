6030b6326f2210ac4d45e0878a2ce5554436893f
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-07-13 17:45:37 +0900

    -initial commit

---
+++ ys-simple-google-analytics.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Ys Simple Google Analytics
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ys-simple-google-analytics
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ys_Simple_Google_Analytics
+ */
