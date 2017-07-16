43ec66f5deb6a1fe3302fab6c24ff222916778e7
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-07-16 14:36:16 +0900

    add: some tests

---
+++ tests/test-get-ga-script-tag.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Create GA Tag
+ *
+ * @package Ys_Simple_Google_Analytics
+ */
+
+/**
+ * Sample test case.
+ */
+class Get_GA_Script_Tag_Test extends WP_UnitTestCase {
+
+	/**
+	 * GAタグ生成のテスト
+	 */
+	function test_get_the_google_analytics_tag() {
+		update_option( 'YSSGA_GA_Tracking_ID', 'UA-12345678-0' );
+		$html = get_the_google_analytics_tag();
+		// 出力されるはずのscriptタグ
+		$result = <<<EOD
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-12345678-0', 'auto');
+ga('send', 'pageview');
+</script>
+EOD;
+		$this->assertEquals( $result, $html );
+	}
+
+	/**
+	 * 設定が空の場合のテスト
+	 */
+	function test_get_the_google_analytics_tag_設定が空の場合() {
+		update_option( 'YSSGA_GA_Tracking_ID', '' );
+		$html = get_the_google_analytics_tag();
+		$this->assertEquals( '', $html );
+	}
+}
