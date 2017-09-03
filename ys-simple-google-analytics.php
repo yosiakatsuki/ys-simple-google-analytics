40d117e74c45435957cc5f1676c731d8078e6e20
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-09-03 10:03:59 +0900

    add .pot file

--- ys-simple-google-analytics.php
+++ ys-simple-google-analytics.php
@@ -94,7 +94,7 @@ add_action( 'admin_init', 'yssga_admin_init' );
  */
 function yssga_options_page() {
 	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		wp_die( __( 'You do not have sufficient permissions to access this page.', 'ys-simple-google-analytics' ) );
 	}
 	?>
 
