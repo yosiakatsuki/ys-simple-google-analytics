789f038e6e2bf53bdd7a415d1dfd0cec4c51d619
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-09-03 10:08:19 +0900

    fix error #14

--- ys-simple-google-analytics.php
+++ ys-simple-google-analytics.php
@@ -57,7 +57,7 @@ function yssga_get_the_google_analytics_tag() {
 	$tracking_code = apply_filters( 'yssga_ga_tracking_code', $tracking_code );
 
 	$tag = <<<EOD
-<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');{$tracking_cod}</script>
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');{$tracking_code}</script>
 EOD;
 	return $tag . PHP_EOL;
 }
